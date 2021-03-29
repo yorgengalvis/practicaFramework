c0fbcc837aef0a5bed5c9388f11158f998a22ebe
Yorgen Galvis <yorgengalvis2000gmail.com>
2021-03-28 21:13:41 -0500

    Ultima modificacion

--- js/js.js
+++ js/js.js
@@ -11,12 +11,14 @@ $("#partidos").click(function(event) {
     $("#contenedor").load('partidos.html');
 });
 
+
+
 var part="";
 
 $.getJSON('data/partidos.json',function(datos){
     $("#contenido-tabla").html('');
     $.each(datos["partidos"],function(idx,partido){
-        $("#contenido").append("<tr>"+"<td>"+partido[fecha]+"</td><td>"+partido["equipo1"]+"</td>"+
+        $("#contenido-tabla").append("<tr>"+"<td>"+partido[fecha]+"</td><td>"+partido["equipo1"]+"</td>"+
         +partido["equipo2"]+"</td>"+
     "</tr>");
     });
