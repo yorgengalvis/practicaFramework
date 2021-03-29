2ade28ce8adba2efa5d9bef4a88a51eb37b789bc
Yorgen Galvis <yorgengalvis2000gmail.com>
2021-03-28 20:47:34 -0500

    Add view partidos

--- js/js.js
+++ js/js.js
@@ -5,3 +5,15 @@ $("#selh3").click(function(event) {
 $("#contacto").click(function(event) {
     $("#contenedor").load('forms/contacto.html');
 });
+
+var part="";
+
+$.getJSON('partidos.json',function(datos){
+    $("#contenido").html('');
+    $.each(datos["partidos"],function(idx.partido){
+        $("#contenido").append("<tr>"+"<td>"+partido[fecha]+"</td><td>"+partido["equipo1"]+"</td><td>
+        +partido["equipo2"]+"</td>"+
+    </tr>");
+    });
+});
+
